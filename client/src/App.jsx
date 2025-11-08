886d4ca70f14574b3d1b6aae8b3f60d295aec6e0
Aditya Shenvi <adityashenvi2050@gmail.com>
2025-11-08 23:46:30 +0530

    v0.016

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import Dashboard from './pages/Dashboard.jsx'
+import { Home } from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="ai" element= {<Layout />} />
+        <Route index element= {<Dashboard />} />
+      </Routes>
+    </div>
   )
 }
 
